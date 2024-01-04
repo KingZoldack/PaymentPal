fc7a0eaa84c8ed103da8fb1cd9861be768f4dfa2
KingZoldack <jeanpaulismael@gmail.com>
2024-01-04 21:14:24 +0000

    Initial Commit
    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PaymentPal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
